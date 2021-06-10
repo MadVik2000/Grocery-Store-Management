b6736d1b1f2e59e9a056e922c5db3f198ef9bac9
MadVik2000 <vkhtgupta@gmail.com>
2021-06-10 21:05:14 +0530

    Model Changes

--- prerequisites.py
+++ prerequisites.py
@@ -11,12 +11,13 @@ mycursor.execute("CREATE TABLE fruits(fruit_id INT AUTO_INCREMENT PRIMARY KEY, f
 
 mycursor.execute("CREATE TABLE lists(list_id INT AUTO_INCREMENT PRIMARY KEY,user_id INT,list_name VARCHAR(50),FOREIGN KEY(user_id)REFERENCES users(user_id));")
 
-mycursor.execute("CREATE TABLE list_items(list_id INT REFERENCES lists(list_id), fruit_id INT REFERENCES fruits(fruit_id), quantity INT);")
-
-mycursor.execute("CREATE TABLE requests(to_id INT references users(user_id),from_id INT references users(user_id));")
+mycursor.execute("CREATE TABLE list_items(items_id INT AUTO_INCREMENT PRIMARY KEY, list_id INT REFERENCES lists(list_id), fruit_id INT REFERENCES fruits(fruit_id), quantity INT);")
 
 mycursor.execute(
-    "CREATE TABLE friends(friend_one INT REFERENCES users(user_id),friend_two INT REFERENCES users(user_id),permission ENUM('y', 'n') DEFAULT 'n');")
+    "CREATE TABLE requests(request_id INT AUTO_INCREMENT PRIMARY KEY, to_id INT references users(user_id),from_id INT references users(user_id));")
+
+mycursor.execute(
+    "CREATE TABLE friends(friends_id INT AUTO_INCREMENT PRIMARY KEY, friend_one INT REFERENCES users(user_id),friend_two INT REFERENCES users(user_id),permission ENUM('y', 'n') DEFAULT 'n');")
 
 myquery = "insert into fruits(fruit_name,fruit_price) values (%s, %s)"
 fruit_list = [('apple',30),('banana',50),('grapes', 60),('guava',40),('litchi',70),('mango',120),
